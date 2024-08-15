9e4b0e83a856168a54845186c9f2b4565319c7ed
Pulsaaar <anatoliykuragin.litra@proton.me>
2024-08-15 15:49:47 +0300

    Initial commit

--- gooddeeds-api/src/main.ts
+++ gooddeeds-api/src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-      origin: '*',
-      methods: 'GET,POST,PUT,DELETE,PATCH',
-      allowedHeaders: 'Content-Type, Authorization',
-      credentials: true,
-  });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
